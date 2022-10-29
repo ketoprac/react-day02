e40ad06c08e9c51729c0d73f025c0ea67c83cecf
unknown <mada.lawliet@gmail.com>
2022-10-29 21:46:30 +0700

    Initial commit

---
+++ src/features/users/EditUser.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import Button from "../../components/Button";
+import TextField from "../../components/TextField";
+import { editUser } from "./userSlice";
+import { v4 as uuidv4 } from 'uuid';
+
+const EditUser = () => {
+  const dispatch = useDispatch();
+  const params = useParams();
+  const users = useSelector((store) => store.users);
+  const navigate = useNavigate();
+  const existingUser = users.filter((user) => user.id === params.id);
+  const { name, email } = existingUser[0];
+  const [values, setValues] = useState({
+    name,
+    email,
+  });
+
+  const handleEditUser = () => {
+    setValues({ name: "", email: "" });
+    dispatch(
+      editUser({
+        id: params.id,
+        name: values.name,
+        email: values.email,
+      })
+    );
+    navigate("/");
+  };
+
+  return (
+    <div className="mt-10 max-w-xl mx-auto">
+      <TextField
+        label="Name"
+        value={values.name}
+        onChange={(e) => setValues({ ...values, name: e.target.value })}
+        inputProps={{ type: "text", placeholder: "John Doe" }}
+      />
+      <br />
+      <TextField
+        label="Email"
+        value={values.email}
+        onChange={(e) => setValues({ ...values, email: e.target.value })}
+        inputProps={{ type: "email", placeholder: "john@mail.com" }}
+      />
+      <Button onClick={handleEditUser}>Edit</Button>
+    </div>
+  );
+};
+
+export default EditUser;
